683540472c2ce913c4ca014c74cff38008be6840
Seeta Ram <Seeta Ram@SEETARAMNAGORE>
2024-02-29 11:59:27 +0530

    authentication

--- Auth0Authentication/Program.cs
+++ Auth0Authentication/Program.cs
@@ -1,7 +1,102 @@
+
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text.Json;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+    var domain = $"https://{builder.Configuration["Authentication:Domain"]}/";
+    options.Authority = domain;
+    options.Audience = builder.Configuration["Authentication:Audience"];
+});
+
+
+//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.OperationFilter<SecurityRequirementsOperationFilter>();
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Implicit = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri($"https://{builder.Configuration["Authentication:Domain"]}/authorize?audience={builder.Configuration["Authentication:Audience"]}"),
+                TokenUrl = new Uri($"https://{builder.Configuration["Authentication:Domain"]}/oauth/token")
+            }
+        }
+    });
+
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
+        c.OAuthClientId(builder.Configuration["Authentication:ClientId"]);
+        c.OAuthUsePkce();
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+        
+
+
+
+
+
+
+
+/*using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
+        options.Audience = builder.Configuration["Auth0:Audience"];
+    });
 
 
 // Add services to the container
@@ -38,12 +133,6 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/";
-        options.Audience = builder.Configuration["Auth0:Audience"];
-    });
 
 builder.Services.AddLogging();
 builder.Services.AddAuthorization(options =>
@@ -82,3 +171,4 @@ app.UseEndpoints(endpoints =>
 });
 
 app.Run();
+*/
